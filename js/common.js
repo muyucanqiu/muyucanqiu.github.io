3d9e8992f5d1aebd57b7cb474e44345a796b4722
canqiu <401135391@qq.com>
2015-11-24 20:09:34 +0800

    add

---
+++ js/common.js
@@ -0,0 +1,86 @@
+//鼠标滚轮
+function addWheel(obj, fn)
+{
+	if (window.navigator.userAgent.toLowerCase().indexOf('firefox') != -1)
+	{
+		obj.addEventListener('DOMMouseScroll', function (ev){
+				var down=ev.detail>0 ? true : false;
+				fn(down);
+		}, false);
+	}
+	else
+	{
+		obj.onmousewheel=function (){
+			var down=event.wheelDelta>0 ? false : true;
+			fn(down);
+		};
+	}
+}
+
+
+
+function addEvent(obj, sEv, fn)
+{
+	if (obj.addEventListener)
+	{
+		obj.addEventListener(sEv, fn, false);
+	}
+	else
+	{
+		obj.attachEvent('on'+sEv, fn);
+	}
+}
+
+function removeEvent(obj, sEv, fnName)
+{
+	if (obj.removeEventListener)
+	{
+		obj.removeEventListener(sEv, fnName, false);
+	}
+	else
+	{
+		obj.detachEvent('on'+sEv, fnName);
+	}
+}
+
+function ready(fn)
+{
+	if (document.addEventListener)
+	{
+		document.addEventListener('DOMContentLoaded', fn, false);
+	}
+	else
+	{
+		document.attachEvent('onreadystatechange', function (){
+			if (document.readyState == 'complete')
+			{
+				fn();
+			}
+		});
+	}
+}
+
+function getByClass(oParent, sName)
+{
+	if (oParent.getElementsByClassName)
+	{
+		return oParent.getElementsByClassName(sName);
+	}
+	else
+	{
+		var aChild=oParent.getElementsByTagName('*');
+		var aRes=[];
+		
+		for (var i=0; i<aChild.length; i++)
+		{
+			var obj=aChild[i];
+			var aTmp=obj.className.split(' ');
+			if (findInArr(aTmp, sName) == true)
+			{
+				aRes.push(obj);
+			}
+		}
+
+		return aRes;
+	}	
+}
---
+++ js/index.js
@@ -0,0 +1,115 @@
+ready(function(){
+	var oBox=document.getElementById('box');
+	var oNav=getByClass(document,'nav')[0];
+	var aLi=oNav.getElementsByTagName('li');
+	var oMain=document.getElementById('main');
+	var aMain=getByClass(document,'box');
+	var top=0;
+	var H=document.documentElement.clientHeight;
+	var oC=document.getElementById('contact');
+	var oN=document.getElementById('number');
+	var oE=document.getElementById('email');
+
+	oC.onmouseover=function(){
+		oN.style.top=-50+'px';
+		oE.style.top=-50+'px';
+		oE.style.left=500+'px';
+	}
+	oC.onmouseout=function(){
+		oN.style.top=0;
+		oE.style.top=0;
+		oE.style.left=500+'px';
+	}
+
+	oBox.style.height=H+'px';
+	oMain.style.height=H+'px';
+	for(var i=0;i<aMain.length;i++){
+		aMain[i].style.height=H+'px';
+	}
+
+	for(var i=0;i<aLi.length;i++){
+		+function(index){
+			aLi[i].onclick=function(){
+				oMain.style.top=-H*index+'px';
+				top=-H*index;
+			}
+			aLi[i].onmouseover=function(){
+				this.classList.add('hover')
+			}
+			aLi[i].onmouseout=function(){
+				this.classList.remove('hover')
+			}
+		}(i)	
+	}
+	//鼠标滑轮
+	addWheel(oMain, function (down){
+		if (down)
+		{
+			// 下
+			top-=H;
+		}
+		else
+		{
+			top+=H;
+		}
+		if (top > 0)
+		{
+			top=0;
+		}
+		else if (top < -H*3)
+		{
+			top=-H*3;
+		}
+
+		oMain.style.top=top+'px';
+	});
+
+	//Js部分
+	+function(){
+		var oJs=getByClass(document,'js')[0];
+		var oTop=getByClass(oJs,'top')[0];
+		var oBottom=getByClass(oJs,'bottom')[0];
+		var aLi=oTop.getElementsByTagName('li');
+		var aLiB=oBottom.getElementsByTagName('li');
+		var aText=getByClass(oTop,'text');
+		var aTextB=getByClass(oBottom,'text');
+
+		for(var i=0;i<aLi.length;i++){
+			aLi[i].index=i;
+			aLi[i].onmouseover=function(){
+				aText[this.index].style.bottom=0;
+			}
+			aLi[i].onmouseout=function(){
+				aText[this.index].style.bottom=-80+'px';
+			}
+		}
+		for(var i=0;i<aLiB.length;i++){
+			aLiB[i].index=i;
+			aLiB[i].onmouseover=function(){
+				aTextB[this.index].style.bottom=0;
+			}
+			aLiB[i].onmouseout=function(){
+				aTextB[this.index].style.bottom=-80+'px';
+			}
+		}
+	}();
+	
+	
+	//H5部分
+	+function(){
+		var oH5=getByClass(document,'h5')[0];
+		var aLi=oH5.getElementsByTagName('li');
+		var aText=getByClass(oH5,'text');
+
+		for(var i=0;i<aLi.length;i++){
+			aLi[i].index=i;
+			aLi[i].onmouseover=function(){
+				aText[this.index].style.bottom=0;
+			}
+			aLi[i].onmouseout=function(){
+				aText[this.index].style.bottom=-100+'px';
+			}
+		}
+	}()	
+})
+	
